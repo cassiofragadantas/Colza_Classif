4b34b176cac3fb262b550bcbd00880dbaa7b5f3c
Cassio F. Dantas <cassiofragadantas@gmail.com>
2022-11-17 19:03:06 +0100

    Adding data interpolation to fill missing values. Also polishing prints.

--- gen_data.py
+++ gen_data.py
@@ -16,6 +16,7 @@ from matplotlib import pyplot as plt
 
 year = 2018  # Year of interest. Options are 2018, ..., 2020
 show_plots = True
+interpolate = True
 
 
 def check_duplicates(df, id='csv'):
@@ -38,14 +39,87 @@ def check_duplicates(df, id='csv'):
     return df
 
 
+### NDVI DATA ###
+print('\n===== ADDING NDVI DATA =====\n')
+path1 = f'./Colza_DB/Plot/{year}/NDVI/ndvi{year-1}_wgs84_Cipan{year}/'
+path2 = f'./Colza_DB/Plot/{year}/NDVI/ndvi{year}_wgs84_Cipan{year}/'
+filename = f'mean_ndvi_wgs84_ndvi_wgs84.csv'
+# Load csv from year and year-1
+df1 = pd.read_csv(path1 + filename, delimiter=';', index_col=0)
+df2 = pd.read_csv(path2 + filename, delimiter=';', index_col=0)
+
+# Check if indexes match
+# Compare year-1 to year
+if not df1.index.equals(df2.index):
+    print(f'WEIRD! Indexes in {year-1} do not match those in {year}!')
+
+# Check for duplicated indexes
+df1 = check_duplicates(df1, year-1)
+df2 = check_duplicates(df2, year)
+
+### CONCATENATE AND TRIM MONTHS OF INTEREST ###
+# Concatenate year and year-1
+df = pd.concat([df1, df2], axis=1, join='outer')
+# Sort by date (should already be sorted)
+df = df.sort_index(axis=1, ascending=True)
+# Keeping only months of interest (Oct/N-1 to July/N)
+df.columns = pd.to_datetime(df.columns)
+date_mask = (df.columns >= str(year-1) + '-10-01') \
+    & (df.columns < str(year)+'-08-01')
+df = df.loc[:, date_mask]
+
+# Missing data visualization
+zeros_per_row = np.count_nonzero(X_NDVI, axis=1)
+zeros_per_column = np.count_nonzero(X_NDVI, axis=0)
+missing_ratio = zeros_per_row.sum()/X_NDVI.size
+if show_plots:
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle(f'NDVI data, {missing_ratio*100:.1f}% missing')
+    # Zeros per row
+    ax1.hist(zeros_per_row, bins=range(X_NDVI.shape[1]+1))
+    ax1.set_title('Zero entries per row')
+    ax1.set(xlabel='# Missing time entries', ylabel='# Rows')
+    # Zeros per column
+    w = round(X_NDVI.shape[0]/50)  # bin width
+    ax2.hist(zeros_per_column, bins=np.arange(0, X_NDVI.shape[0] + w, w))
+    ax2.set_title('Zero entries per column')
+    ax2.set(xlabel='# Missing samples', ylabel='# Columns')
+    # View data as colormap
+    # plt.imshow(X_NDVI == 0, aspect='auto',cmap='Greys')  # Visualize data
+    # plt.xlabel('Time index'), plt.ylabel('Sample index')
+
+### INTERPOLATION ###
+df = df.replace(0, np.nan)
+if interpolate:
+    print('\nINTERPOLATING DATA THROUGH TIME')
+    df = df.interpolate(method='time', axis=1)
+    df = df.interpolate(method='time', axis=1,
+                        limit_direction='backward')
+# Missing data
+allNaN = df.isnull().all(1)
+if any(allNaN):
+    print(f'WEIRD! {allNaN.sum()} all NaN rows. Removing...')
+    df = df.loc[~allNaN]
+missing_ratio = sum(df.isnull().sum())/df.size
+print(f'{missing_ratio}% missing data remaining.')
+
+NDVI_df = df
+NDVI_index = df.index
+print(f'\nNDVI data dimension: {NDVI_df.shape}')
+
+### SAR DATA ###
+print('\n===== ADDING SAR DATA =====')
 X_SAR = []
 default_idx = pd.DataFrame().index
-merged_idx = pd.DataFrame().index
+SAR_index = pd.DataFrame().index
 for polarization in ['VV', 'VH']:
     allOrbs_df = pd.DataFrame()
+
+    print(f'\n----- TYPE {polarization} -----')
+
     for orb in [81, 8, 30]:
 
-        print(f'\nYear {year}, orbit {orb}, type {polarization}:')
+        print(f'\nORBIT {orb}:')
 
         ### LOAD DATA ###
         # Place your data at './Colza_DB/Plot/'
@@ -100,7 +174,7 @@ for polarization in ['VV', 'VH']:
         # Concatenate year and year-1
         df = pd.concat([df1, df2], axis=1, join='outer')
         # Sort by date (should already be sorted)
-        df.sort_index(axis=1, ascending=True)
+        df = df.sort_index(axis=1, ascending=True)
         # Keeping only months of interest (Oct/N-1 to July/N)
         df.columns = pd.to_datetime(df.columns)
         date_mask = (df.columns >= str(year-1) + '-10-01') \
@@ -112,8 +186,8 @@ for polarization in ['VV', 'VH']:
         print(f'Data dimensions: ', data.shape)
 
         ### HANDLE MISSING DATA (ZEROS) ###
-        zeros_per_row = np.count_nonzero(data, axis=1)
-        zeros_per_column = np.count_nonzero(data, axis=0)
+        zeros_per_row = np.count_nonzero(data == 0, axis=1)
+        zeros_per_column = np.count_nonzero(data == 0, axis=0)
         missing_ratio = zeros_per_row.sum()/data.size
         if show_plots:
             fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -134,86 +208,70 @@ for polarization in ['VV', 'VH']:
             # plt.xlabel('Time index'), plt.ylabel('Sample index')
 
         ### MERGE ORBITS ###
-        print('\nMERGING ORBITS')
-        print(f'Shape allOrbd_df: {allOrbs_df.shape}')
-        print(f'Shape current df: {df.shape}')
+        print('\nMerging orbits...')
         if not df.index.equals(allOrbs_df.index):
             print(f'WEIRD! Indexes do not match among orbits!')
-
         allOrbs_df = pd.concat([allOrbs_df, df], axis=1, join='outer')
-        allOrbs_df.sort_index(axis=1, ascending=True)  # Sort by date
+        allOrbs_df = allOrbs_df.sort_index(axis=1, ascending=True)
+
+    # Harmonize indexes with NDVI data
+    print(f'\nRestricting SAR data to NDVI indexes')
+    if all(NDVI_index.isin(allOrbs_df.index)):
+        allOrbs_df = allOrbs_df.loc[NDVI_index]
+    else:
+        nb_missing = sum(~NDVI_index.isin(allOrbs_df.index))
+        print(f'{nb_missing} NDVI index missing on SAR data. Taking intersection.')
+        allOrbs_df = allOrbs_df.loc[NDVI_index.intersects(allOrbs_df.index)]
+    print(f'All orbits dimensions: {allOrbs_df.shape}')
+
+    ### INTERPOLATION ###
+    allOrbs_df = allOrbs_df.replace(0, np.nan)
+    if interpolate:
+        print('\nINTERPOLATING DATA THROUGH TIME')
+        allOrbs_df = allOrbs_df.interpolate(method='time', axis=1)
+        allOrbs_df = allOrbs_df.interpolate(method='time', axis=1,
+                                            limit_direction='backward')
+    # Missing data
+    allNaN = allOrbs_df.isnull().all(1)
+    if any(allNaN):
+        print(f'WEIRD! {allNaN.sum()} all NaN rows. Removing...')
+        allOrbs_df = allOrbs_df.loc[~allNaN]
+    missing_ratio = sum(allOrbs_df.isnull().sum())/allOrbs_df.size
+    print(f'{missing_ratio}% missing data remaining.')
 
     ### STACKING POLARIZATIONS ###
     print('\nSTACKING POLARIZATIONS')
-    if merged_idx.empty:
-        merged_idx = allOrbs_df.index  # Default indexes for comparison
+    if SAR_index.empty:
+        SAR_index = allOrbs_df.index  # Default indexes for comparison
     else:
-        if not allOrbs_df.index.equals(merged_idx):
+        if not allOrbs_df.index.equals(SAR_index):
             print(f'WEIRD! Indexes do not match between polarizations !')
-    print(f'All orbits dimensions: {allOrbs_df.shape}')
-
     X_SAR.append(allOrbs_df.to_numpy())
+
+print(f'\n-------------------------------')
 
 #### SAR DATA ###
 X_SAR = np.stack(X_SAR, axis=-1)
 # X_SAR = 10*np.log10(X_SAR) # Convert to dB
 print(f'\nFinal SAR data dimension: {X_SAR.shape}')
 
-### NDVI DATA ###
-print('\nADDING NDVI DATA')
-path1 = f'./Colza_DB/Plot/{year}/NDVI/ndvi{year-1}_wgs84_Cipan{year}/'
-path2 = f'./Colza_DB/Plot/{year}/NDVI/ndvi{year}_wgs84_Cipan{year}/'
-filename = f'mean_ndvi_wgs84_ndvi_wgs84.csv'
-# Load csv from year and year-1
-df1 = pd.read_csv(path1 + filename, delimiter=';', index_col=0)
-df2 = pd.read_csv(path2 + filename, delimiter=';', index_col=0)
-
-# Check if indexes match
-# Compare year-1 to year
-if not df1.index.equals(df2.index):
-    print(f'WEIRD! Indexes in {year-1} do not match those in {year}!')
-default_idx = df1.index.copy()
-
-# Check for duplicated indexes
-df1 = check_duplicates(df1, year-1)
-df2 = check_duplicates(df2, year)
-
-### CONCATENATE AND TRIM MONTHS OF INTEREST ###
-# Concatenate year and year-1
-df = pd.concat([df1, df2], axis=1, join='outer')
-# Sort by date (should already be sorted)
-df.sort_index(axis=1, ascending=True)
-# Keeping only months of interest (Oct/N-1 to July/N)
-df.columns = pd.to_datetime(df.columns)
-date_mask = (df.columns >= str(year-1) + '-10-01') \
-    & (df.columns < str(year)+'-08-01')
-df = df.loc[:, date_mask]
-X_NDVI = df.to_numpy()
-
+#### NDVI DATA ###
+# Harmonize indexes with SAR data
+print(f'\nRestricting NDVI data to SAR indexes')
+if all(SAR_index.isin(NDVI_index)):
+    NDVI_df = NDVI_df.loc[SAR_index]
+else:
+    nb_missing = sum(~SAR_index.isin(NDVI_index))
+    print(f'{nb_missing} SAR index missing on NDVI data. Taking intersection.')
+    NDVI_df = NDVI_df.loc[SAR_index.intersects(NDVI_index)]
+NDVI_index = NDVI_df.index
+X_NDVI = NDVI_df.to_numpy()
 print(f'\nFinal NDVI data dimension: {X_NDVI.shape}')
 
-zeros_per_row = np.count_nonzero(X_NDVI, axis=1)
-zeros_per_column = np.count_nonzero(X_NDVI, axis=0)
-missing_ratio = zeros_per_row.sum()/X_NDVI.size
-if show_plots:
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.suptitle(f'NDVI data, {missing_ratio*100:.1f}% missing')
-    # Zeros per row
-    ax1.hist(zeros_per_row, bins=range(X_NDVI.shape[1]+1))
-    ax1.set_title('Zero entries per row')
-    ax1.set(xlabel='# Missing time entries', ylabel='# Rows')
-    # Zeros per column
-    w = round(X_NDVI.shape[0]/50)  # bin width
-    ax2.hist(zeros_per_column, bins=np.arange(0, X_NDVI.shape[0] + w, w))
-    ax2.set_title('Zero entries per column')
-    ax2.set(xlabel='# Missing samples', ylabel='# Columns')
-    # View data as colormap
-    # plt.imshow(X_NDVI == 0, aspect='auto',cmap='Greys')  # Visualize data
-    # plt.xlabel('Time index'), plt.ylabel('Sample index')
-
+assert SAR_index.equals(NDVI_index), 'SAR and NDVI indexes should be equal'
 
 ### LABELS ###
-print('\nTREATING LABELS')
+print('\n===== TREATING LABELS =====\n')
 # Place your data at './Colza_DB/ID/'
 # E.g.: './Colza_DB/ID/gid_5000_2018.csv'
 path = './Colza_DB/ID/'
@@ -224,8 +282,10 @@ print(f'Total listed samples: {gid.shape[0]}')
 # Check for duplicated indexes
 gid = check_duplicates(gid, 'gid')
 
-# Filter rows corresponding to default indexes
-gid = gid.loc[allOrbs_df.index]
+# Restric to default indexes (from SAR and NDVI data)
+print(f'Restricting to data (SAR/NDVI) indexes')
+gid = gid.loc[SAR_index]
+
 print(f'Total available samples: {gid.shape[0]}')
 
 # Class histograms
@@ -235,8 +295,11 @@ if show_plots:
     class_counts[:20].plot.bar()
     plt.title(
         f'Histogram of more common classes (20 out of {len(class_counts)})')
+    print(
+        f'{class_counts["CZH"]} (winter) + {class_counts["CZP"]} (spring) colza samples.')
 
 y = gid["CODE_CULTU"].to_numpy()
 
 ### SAVE DATA ###
-np.savez(f'Colza_DB/Colza_data_{year}', X_SAR=X_SAR, X_NDVI=X_NDVI, y=y)
+np.savez(f'Colza_DB/Colza_data_{year}', X_SAR=X_SAR,
+         X_NDVI=X_NDVI, y=y, id_parcels=SAR_index)
