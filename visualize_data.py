d4ba28f0c4718d398a8f42c6dc122dadf0b65a32
Cassio F. Dantas <cassiofragadantas@gmail.com>
2022-12-23 21:27:25 +0100

    Completing visualization

--- visualize_data.py
+++ visualize_data.py
@@ -1,19 +1,25 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.dates import DateFormatter
+import pandas as pd
 
+# BUG TESTING
+# plt.plot(colza_SAR[:,43,-1])
 
 def main():
 
     # fig1, axs1 = plt.subplots(5, 1, constrained_layout=True)
     # fig1.suptitle('Colza', fontsize=16)
 
+    years = [2018, 2019, 2020]
     titles_SAR = ['VV', 'VV grid', 'VH', 'VH grid']
 
 
-    for year in [2018, 2019, 2020]:
+    for year in years:
         dataset=np.load(f'Colza_DB/Colza_data_{year}.npz', allow_pickle=True)
         X_SAR, X_NDVI, y_multi, id_p=dataset["X_SAR"], dataset["X_NDVI"], dataset["y"], dataset["id_parcels"]
-
+        dates_SAR = pd.to_datetime(dataset["dates_SAR"]) - pd.DateOffset(years=year-2018)
+        dates_NDVI = pd.to_datetime(dataset["dates_NDVI"]) - pd.DateOffset(years=year-2018)
 
         # --- Colza profiles ---
         # SAR
@@ -23,16 +29,20 @@ def main():
         std=np.std(colza_SAR, axis=0)
         for k, title in enumerate(titles_SAR):
             plt.figure(k)
+            # X-axis: Index number
+            plt.subplot(2,1,1)            
             plt.title('Colza ' + title)
-            plt.plot(mean[:, k])
-            plt.fill_between(range(n_times), mean[:, k] +
-                                std[:, k], mean[:, k]-std[:, k], alpha=0.5)        
-        # plt.figure(fig1)
-        # for k, title in enumerate(axs1):
-        #     axs1[k].set_title(title)
-        #     axs1[k].plot(mean[:, k])
-        #     axs1[k].fill_between(range(n_times), mean[:, k] +
-        #                         std[:, k], mean[:, k]-std[:, k], alpha=0.5)
+            plt.plot(mean[:, k],label=year)
+            plt.fill_between(np.arange(n_times), mean[:, k] +
+                                std[:, k], mean[:, k]-std[:, k], alpha=0.5)
+            plt.legend()
+            # X-axis: Dates
+            ax = plt.subplot(2,1,2)            
+            plt.plot(dates_SAR, mean[:, k])
+            plt.fill_between(dates_SAR, mean[:, k] +
+                                std[:, k], mean[:, k]-std[:, k], alpha=0.5)
+            plt.xticks(rotation = 45) # Rotates X-Axis Ticks by 45-degrees
+            ax.xaxis.set_major_formatter(DateFormatter("%b"))
 
         # NDVI
         colza_NDVI=X_NDVI[y_multi == 'CZH']
@@ -40,15 +50,34 @@ def main():
         mean=np.mean(colza_NDVI, axis=0)
         std=np.std(colza_NDVI, axis=0)
         plt.figure(5)
+        # X-axis: Index number
+        plt.subplot(2,1,1)
         plt.title('NDVI')
-        plt.plot(mean)
-        plt.fill_between(range(n_times), mean+std, mean-std, alpha=0.5)
+        plt.plot(mean,label=year)
+        plt.fill_between(np.arange(n_times), mean+std, mean-std, alpha=0.5)
+        plt.legend()
+        # X-axis: Dates
+        ax = plt.subplot(2,1,2)
+        plt.plot(dates_NDVI, mean)
+        plt.fill_between(dates_NDVI, mean+std, mean-std, alpha=0.5)
+        plt.xticks(rotation = 45) # Rotates X-Axis Ticks by 45-degrees
+        ax.xaxis.set_major_formatter(DateFormatter("%b"))
 
         # colza_np = colza[:,:,0]
         # colza_norm = colza_np/np.sqrt((colza_np * colza_np).sum(axis=1))[:,np.newaxis] # normalize rows
         # cov_mtx = colza_norm.dot(colza_norm.T)
 
-        # Dates
+        # Acquisition Dates
+        plt.figure(6, figsize=(6,1))
+        ax = plt.subplot(1,1,1)
+        ax.scatter(dates_NDVI, [0.5*(year-2017)]*len(dates_NDVI), marker='o', s=30, alpha=0.5)
+        ax.xaxis.set_major_formatter(DateFormatter("%b"))
+        ax.yaxis.set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.xaxis.set_ticks_position('bottom')
+        ax.get_yaxis().set_ticklabels([])
 
 if __name__ == "__main__":
     main()
